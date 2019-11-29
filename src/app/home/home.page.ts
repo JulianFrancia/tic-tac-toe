50816fdc041a74d677cf0ebe5d16f511f2fa15f8
Julian <julianf@bombieri.com.ar>
2019-11-29 17:06:36 -0300

    myApp

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public symbol: string;
+  public squares: string[];
+  public turn: string;
+  public random:number[];
 
-  constructor() {}
+  constructor() {
+    this.turn= 'player1'
+    this.symbol= '';
+    this.squares= Array(9).fill(null);
+    this.random=[0,1,2,3,4,5,6,7,8]
+  }
 
+  show(pos){
+    if(this.turn == 'player1'){
+      if(!this.squares[pos]){
+        this.squares.splice(pos,1,'X')
+        let rand = this.random[Math.floor(Math.random() * this.random.length)]
+        this.squares.splice(rand,1,'O')
+      }
+    }
+  }
 }
+/* 
+else if(this.turn == 'player2'){
+      if(!this.squares[pos]){
+        this.squares.splice(pos,1,'O')
+        this.turn= 'player1'
+      }
+    }
+*/
