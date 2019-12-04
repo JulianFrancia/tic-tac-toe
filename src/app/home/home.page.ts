e9eaada8d94cc51703eb3af5c4f5165f670b01ee
Julian <julianfrancia110@gmail.com>
2019-12-04 00:04:00 -0300

    Bot2 = mayor dificultad

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -47,12 +47,12 @@ export class HomePage {
       if(this.turn == 'player2'){
         let rand = this.random[Math.floor(Math.random() * this.random.length)];
         if(this.squares[rand] == null ){
-          this.squares.splice(rand,1,'O')
-          this.random.splice(rand,1)
+          this.squares.splice(rand,1,'O');
+          this.random.splice(rand,1);
         } else{
-          let found= this.squares.find(element => element == null)
-          rand= this.squares.indexOf(found)
-          this.squares[rand]='O'
+          let found= this.squares.find(element => element == null);
+          rand= this.squares.indexOf(found);
+          this.squares[rand]='O';
         }
         this.changeTurn();
         this.checkWinner();
@@ -68,7 +68,7 @@ export class HomePage {
             this.changeTurn();
             this.checkWinner();  
           }
-        } this.bot();
+        } this.bot2();
       }
   }
   tryAgain(){
@@ -93,6 +93,45 @@ export class HomePage {
         this.gameOver = true;
         this.winner = this.squares[line[0]];
       }
+    }
+  }
+
+  bot2(){
+    if(!this.gameOver){
+      if(this.turn == 'player2'){
+        if(!this.squares[4]){
+          this.squares[4]= 'O'
+        } else if(!this.squares[0] && !this.squares[2] && !this.squares[6] && !this.squares[8]){
+                let corners= [0, 2, 6, 8];
+                let rand= corners[Math.floor(Math.random() * corners.length)];
+                this.squares[rand]= 'O'
+        } else{
+          let winnerLines= [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,4,8],
+            [2,4,6],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8]
+          ];
+          for(let line of winnerLines){
+            if(this.squares[line[0]]=='X' && this.squares[line[1]]=='X' && !this.squares[line[2]]){
+              console.log('adentro1')
+              this.squares[line[2]]= 'O'
+            } else if(this.squares[line[0]]=='X' && !this.squares[line[1]] && this.squares[line[2]]=='X'){
+              this.squares[line[1]]= 'O'
+              console.log('adentro2')
+            } else if(!this.squares[line[0]] && this.squares[line[1]]=='X' && this.squares[line[2]]=='X'){
+              this.squares[line[0]]='O'
+              console.log('adentro3')
+            }
+          }
+        }
+      }
+      this.changeTurn();
+      this.checkWinner();
     }
   }
 }
